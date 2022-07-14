58c6566bf68527dd1d7e6ad9b0129c67ba00d8f9
Damian-Gbad <damesgbad@gmail.com>
2022-07-14 10:40:46 +0000

    Just want to keep a saved copy of the furthest

--- Django/mysite/main/urls.py
+++ Django/mysite/main/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-path("", views.index, name= "index"), 
-path("version1/", views.version1, name= "view 1"), 
+path("<int:id>", views.index, name= "index"), 
 ]
--- Django/mysite/main/views.py
+++ Django/mysite/main/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import ToDoList, Item
 
 # Create your views here.
 
-def index(response):
-    return HttpResponse("<h1>My first django website</h1>")
-
-def version1(response):
-    return HttpResponse("<h1>View one</h1>")
+def index(response, id):
+    ls = ToDoList.objects.get(id=id)
+    return HttpResponse("<h1>%s</h1>" %ls.name)
